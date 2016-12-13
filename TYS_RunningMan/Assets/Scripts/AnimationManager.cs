1091d0a5b77c486e0c3dc094a104c1535659c23b
prettyboyzhuo <769956207@qq.com>
2016-12-13 17:37:49 +0800

    12-13

---
+++ TYS_RunningMan/Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+	[System.Serializable]//让自定义的类有拖拉的界面
+	public class AnimationSet{
+		public AnimationClip animation;
+		public float  speedAnimation =1;
+	}
+
+	public AnimationSet run,jump;
+
+	//委托
+	public delegate void AnimationHandle();
+	public AnimationHandle animationState;
+
+	Animation anim;
+
+	void Start () {
+		anim = GetComponent<Animation> ();//缓存
+		animationState = Run;
+
+	}
+	
+
+	void Update () {
+		if (animationState != null)
+			animationState ();
+	}
+	public void Run(){
+		anim.Play (run.animation.name);
+		anim[run.animation.name].speed = run.speedAnimation;
+	}
+	public void Jump(){
+		anim.Play (jump.animation.name);
+		anim[jump.animation.name].speed = run.speedAnimation;
+	}
+}
